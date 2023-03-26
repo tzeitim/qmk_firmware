febfeb94019e354bb10322557ddfa0977184f041
tzeitim <pedroy.final@gmail.com>
2023-03-26 16:31:27 +0200

    test via

--- keyboards/bastardkb/dilemma/3x5_2/keymaps/via/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/via/keymap.c
@@ -60,7 +60,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
+       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_O, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
                                TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM
